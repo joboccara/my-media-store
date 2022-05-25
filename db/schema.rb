4a28fc953e4bed203328967bd86f2131ab04f416
joboccara <boccaraj@gmail.com>
2022-05-25 14:42:11 +0200

    Add UserItem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_105644) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_25_124109) do
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -19,10 +19,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_25_105644) do
     t.string "kind"
   end
 
+  create_table "user_items", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_user_items_on_item_id"
+    t.index ["user_id"], name: "index_user_items_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_items", "items"
+  add_foreign_key "user_items", "users"
 end
