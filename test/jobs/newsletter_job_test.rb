1bf50c900d27b2dfe064cb18cbe851f27291c02f
joboccara <boccaraj@gmail.com>
2022-05-30 13:05:43 +0200

    Add use case for image

--- test/jobs/newsletter_job_test.rb
+++ test/jobs/newsletter_job_test.rb
@@ -6,14 +6,14 @@ class NewsletterJobTest < ActiveSupport::TestCase
   test 'it sends a newsletter with the existing products' do
     repo.create_book(title: 'Item 1', content: 'Super item', category: 'star-wars', page_count: 42)
     repo.create_video(title: 'Item 2', content: 'Great item', category: 'star-wars', duration: 120)
-    repo.create_book(title: 'Item 3', content: 'Big item', category: 'other', page_count: 30)
+    repo.create_image(title: 'Item 3', content: 'Big item', category: 'other', width: 800, height: 600)
 
     expected_mail_content = <<~MAIL
     star-wars
     Item 1 - book - 42 pages
     Item 2 - video - 120 seconds
     other
-    Item 3 - book - 30 pages
+    Item 3 - image - 800x600
     MAIL
 
     mail_sender = mock
