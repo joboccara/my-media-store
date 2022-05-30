14ef5791e1bee371eaffe0915c63202163eab055
joboccara <boccaraj@gmail.com>
2022-05-30 23:44:05 +0200

    Refactor product repository

--- app/repositories/product_repository.rb
+++ app/repositories/product_repository.rb
@@ -1,12 +1,7 @@
 class ProductRepository
   def get_product(id)
-    item = Item.find(id)
-    raise "Product not found #{id}" if item.nil?
-    book_details = hash_by(item.kind == 'book' ? BookDetail.where(item: item) : [], :item_id)
-    image_details = hash_by(item.kind == 'image' ? ImageDetail.where(item: item) : [], :item_id)
-    video_details = hash_by(item.kind == 'video' ? VideoDetail.where(item: item) : [], :item_id)
-    build_product_dto_from_hashes(item, book_details, image_details, video_details)
-  end
+    build_products([Item.find(id)]).first
+end
 
   def get_product_with_category(category)
     build_products(Item.where(category: category))
@@ -17,94 +12,108 @@ class ProductRepository
   end
 
   def create_book(title:, content:, category:, page_count:)
-    item = Item.create(kind: 'book', title: title, content: content, category: category)
-    details = BookDetail.create(item: item, page_count: page_count)
-    build_product_dto(item, build_book_dto(details))
+    item = Item.create!(kind: 'book', title: title, content: content, category: category)
+    book_details = BookDetail.create!(item: item, page_count: page_count)
+    item_dto(item).merge(book_details_dto(book_details))
   end
 
-  def update_book(id, new_attributes)
-    validate_book_columns(new_attributes.keys)
-    item_attributes, book_details_attributes = new_attributes.partition { |key, _value| key.to_s.in? Item.column_names }
-    Item.update!(id, item_attributes.to_h) if item_attributes.any?
-    if book_details_attributes.any?
-      book_detail = BookDetail.find_by(item_id: id)
-      raise "Product not found #{id}" if book_detail.nil?
-      book_detail.update!(book_details_attributes.to_h)
-    end
+  def update_book(item_id:, **new_attributes)
+    item_attributes, book_details_attributes = partition_book_attributes(new_attributes)
+    Item.update(item_id, item_attributes) if item_attributes.present?
+    BookDetail.update(BookDetail.find_by(item_id: item_id).id, book_details_attributes) if book_details_attributes.present?
   end
 
   def create_image(title:, content:, category:, width:, height:)
-    item = Item.create(kind: 'image', title: title, content: content, category: category)
-    details = ImageDetail.create(item: item, width: width, height: height)
-    build_product_dto(item, build_image_dto(details))
+    item = Item.create!(kind: 'image', title: title, content: content, category: category)
+    image_details = ImageDetail.create!(item: item, width: width, height: height)
+    item_dto(item).merge(image_details_dto(image_details))
+  end
+
+  def update_image(item_id:, **new_attributes)
+    item_attributes, image_details_attributes = partition_image_attributes(new_attributes)
+    Item.update(item_id, item_attributes) if item_attributes.present?
+    ImageDetail.update(ImageDetail.find_by(item_id: item_id).id, image_details_attributes) if image_details_attributes.present?
   end
 
   def create_video(title:, content:, category:, duration:)
-    item = Item.create(kind: 'video', title: title, content: content, category: category)
-    details = VideoDetail.create(item: item, duration: duration)
-    build_product_dto(item, build_video_dto(details))
+    item = Item.create!(kind: 'video', title: title, content: content, category: category)
+    video_details = VideoDetail.create!(item: item, duration: duration)
+    item_dto(item).merge(video_details_dto(video_details))
+  end
+
+  def update_video(item_id:, **new_attributes)
+    item_attributes, video_details_attributes = partition_video_attributes(new_attributes)
+    Item.update(item_id, item_attributes) if item_attributes.present?
+    VideoDetail.update(VideoDetail.find_by(item_id: item_id).id, video_details_attributes) if video_details_attributes.present?
   end
 
   private
 
   def build_products(items)
-    # other option: ActiveRecord::Associations::Preloader (https://thepaulo.medium.com/eager-loading-polymorphic-associations-in-ruby-on-rails-155a356c39d7)
-    grouped_items = items.group_by(&:kind)
-    book_details = hash_by(BookDetail.where(item: grouped_items['book']), :item_id)
-    image_details = hash_by(ImageDetail.where(item: grouped_items['image']), :item_id)
-    video_details = hash_by(VideoDetail.where(item: grouped_items['video']), :item_id)
-    items.map { |item| build_product_dto_from_hashes(item, book_details, image_details, video_details) }
+    book_details = BookDetail.where(item: items).to_a
+    image_details = ImageDetail.where(item: items).to_a
+    video_details = VideoDetail.where(item: items).to_a
+    items.map do |item|
+      belongs_to_item = ->(detail) { detail.item_id == item.id }
+      case item.kind
+      when 'book' then details_dto = book_details_dto(book_details.find(&belongs_to_item))
+      when 'image' then details_dto = image_details_dto(image_details.find(&belongs_to_item))
+      when 'video' then details_dto = video_details_dto(video_details.find(&belongs_to_item))
+      end
+      item_dto(item).merge(details_dto)
+    end
   end
 
-  def hash_by(array, attr)
-    Hash[array.collect { |item| [item[attr], item] }]
-  end
-
-  def build_product_dto_from_hashes(item, book_details, image_details, video_details)
-    details = if item.kind == 'book'
-                build_book_dto(book_details[item.id] || (raise "Missing details for book #{item.id}"))
-              elsif item.kind == 'image'
-                build_image_dto(image_details[item.id] || (raise "Missing details for image #{item.id}"))
-              elsif item.kind == 'video'
-                build_video_dto(video_details[item.id] || (raise "Missing details for video #{item.id}"))
-              else
-                raise "Unknown item kind #{item.kind.inspect}"
-              end
-    build_product_dto(item, details)
-  end
-
-  def build_product_dto(item, details_dto)
+  def item_dto(item)
     {
       id: item.id,
-      kind: item.kind,
       title: item.title,
+      content: item.content,
       category: item.category,
-      content: item.content
-    }.merge(details_dto)
-  end
-
-  def build_book_dto(details)
-    {
-      page_count: details.page_count
+      kind: item.kind
     }
   end
 
-  def build_image_dto(details)
+  def book_details_dto(book_details)
     {
-      width: details.width,
-      height: details.height
+      page_count: book_details.page_count
     }
   end
 
-  def build_video_dto(details)
+  def image_details_dto(image_details)
     {
-      duration: details.duration
+      width: image_details.width,
+      height: image_details.height
     }
   end
 
-  def validate_book_columns(columns)
-    valid_columns = Item.column_names + BookDetail.column_names
-    are_columns_valid = (columns.map(&:to_s) - valid_columns).empty?
-    raise "Invalid columns #{columns.inspect} for books" unless are_columns_valid
+  def video_details_dto(video_details)
+    {
+      duration: video_details.duration
+    }
+  end
+
+  def partition_book_attributes(attributes)
+    partition_attributes attributes, Item, BookDetail
+  end
+
+
+  def partition_image_attributes(attributes)
+    partition_attributes attributes, Item, ImageDetail
+  end
+
+  def partition_video_attributes(attributes)
+    partition_attributes attributes, Item, VideoDetail
+  end
+
+  def partition_attributes(attributes, table_1, table_2)
+    table_1_columns = attributes.keys & table_1.column_names.map(&:to_sym)
+    table_2_columns = attributes.keys & table_2.column_names.map(&:to_sym)
+    unknown_columns = attributes.keys - (table_1_columns + table_2_columns)
+    raise "Unknown attributes: #{unknown_columns}" if unknown_columns.any?
+    [
+      attributes.slice(*table_1_columns),
+      attributes.slice(*table_2_columns)
+    ]
   end
 end
--- test/repositories/product_repository_test.rb
+++ test/repositories/product_repository_test.rb
@@ -3,22 +3,56 @@ require 'test_helper'
 class ProductRepositoryTest < ActiveSupport::TestCase
   repo = ProductRepository.new
 
-  test 'create and get book' do
+  test 'create, get and update book' do
     book = repo.create_book(title: 'title', content: 'content', category: 'category', page_count: 10)
 
     product = repo.get_product(book[:id])
-    assert_equal book[:title], product[:title]
+    assert_equal 'title', product[:title]
+    assert_equal 10, product[:page_count]
 
     products = repo.get_product_with_category(book[:category])
-    assert_equal book[:title], products[0][:title]
-  end
+    assert_equal 'title', products[0][:title]
+    assert_equal 10, products[0][:page_count]
 
-  test 'create and update book' do
-    book = repo.create_book(title: 'title', content: 'content', category: 'category', page_count: 10)
-    repo.update_book(book[:id], title: 'new title', page_count: 20)
-
+    repo.update_book(item_id: book[:id], title: 'new title', page_count: 20)
     new_book = repo.get_product(book[:id])
     assert_equal new_book[:title], 'new title'
     assert_equal new_book[:page_count], 20
+end
+
+  test 'create, get and update image' do
+    image = repo.create_image(title: 'title', content: 'content', category: 'category', width: 800, height: 600)
+
+    product = repo.get_product(image[:id])
+    assert_equal 'title', product[:title]
+    assert_equal 800, product[:width]
+    assert_equal 600, product[:height]
+
+    products = repo.get_product_with_category(image[:category])
+    assert_equal 'title', products[0][:title]
+    assert_equal 800, products[0][:width]
+    assert_equal 600, products[0][:height]
+
+    repo.update_image(item_id: image[:id], title: 'new title', width: 900, height: 700)
+    new_image = repo.get_product(image[:id])
+    assert_equal new_image[:title], 'new title'
+    assert_equal new_image[:width], 900
+  end
+
+  test 'create and get video' do
+    video = repo.create_video(title: 'title', content: 'content', category: 'category', duration: 60)
+
+    product = repo.get_product(video[:id])
+    assert_equal 'title', product[:title]
+    assert_equal 60, product[:duration]
+
+    products = repo.get_product_with_category(video[:category])
+    assert_equal 'title', products[0][:title]
+    assert_equal 60, products[0][:duration]
+
+    repo.update_video(item_id: video[:id], title: 'new title', duration: 120)
+    new_video = repo.get_product(video[:id])
+    assert_equal new_video[:title], 'new title'
+    assert_equal new_video[:duration], 120
   end
 end
