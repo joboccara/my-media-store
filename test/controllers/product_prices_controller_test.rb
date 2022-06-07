db2169026765108ea4fe90587344bdd3c549c93e
joboccara <boccaraj@gmail.com>
2022-06-07 15:10:14 +0200

    price

--- test/controllers/product_prices_controller_test.rb
+++ test/controllers/product_prices_controller_test.rb
@@ -4,12 +4,17 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
   repo = ProductRepository.new
 
   test 'price of a book' do
-    ENV['BOOK_PURCHASE_PRICE']  = '12'
     book = repo.create_book(title: 'Title of Book', content: 'Contents of Book')
 
+    ENV['BOOK_PURCHASE_PRICE']  = '12'
     get product_price_url(book[:id])
-
     price = response.parsed_body
     assert_equal 15, price.to_i
+
+    ENV['BOOK_PURCHASE_PRICE']  = '16'
+    get product_price_url(book[:id])
+    price = response.parsed_body
+    assert_equal 20, price.to_i
+
   end
 end
