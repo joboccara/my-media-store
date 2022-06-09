56714a800b002b60e43caf9fcca8be003563c4bf
joboccara <boccaraj@gmail.com>
2022-06-09 10:35:21 +0200

    it friendly test names

--- test/controllers/product_prices_controller_test.rb
+++ test/controllers/product_prices_controller_test.rb
@@ -8,7 +8,7 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
     Timecop.return
   end
 
-  test 'price of a book based on purchase price' do
+  test 'prices books at +25% margin' do
     book1 = repo.create_book(title: 'Title of Book1', isbn: '1', purchase_price: 12, is_hot: false)
     assert_price_equal 15, get_product_price(book1[:id])
 
@@ -16,7 +16,7 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
     assert_price_equal 20, get_product_price(book2[:id])
   end
 
-  test 'price of a book in the ISBN list' do
+  test 'checks the ISBN list to price books' do
     begin
       CSV.open(Rails.root.join('isbn_list.csv'), 'w') do |csv|
         csv << ['ISBN', 'price']
@@ -36,7 +36,7 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'hot books are fixed during weekdays' do
+  test 'prices hot books at 9.99 during weekdays' do
     book = repo.create_book(title: 'Title of Book', isbn: '9781603095099', purchase_price: 14, is_hot: true)
     Timecop.travel(Time.new(2022, 1, 3)) # Monday
     assert_price_equal 9.99, get_product_price(book[:id])
@@ -50,7 +50,7 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
     assert_price_equal 9.99, get_product_price(book[:id])
   end
 
-  test 'hot books are fixed during weekends' do
+  test 'does not take hot books into account during weekends' do
     begin
       CSV.open(Rails.root.join('isbn_list.csv'), 'w') do |csv|
         csv << ['ISBN', 'price']
@@ -66,17 +66,17 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'images from NationalGeographic are sold at .02$/9600px' do
+  test 'prices images from NationalGeographic .02/9600px' do
     image = repo.create_image(title: 'Title of Image', width: 800, height: 600, source: 'NationalGeographic', format: 'jpg')
     assert_price_equal 1, get_product_price(image[:id])
   end
 
-  test 'images from Getty are sold at 1 when below 1280x720' do
+  test 'prices images from Getty at 1 when below 1280x720' do
     image = repo.create_image(title: 'Title of Image', width: 1280, height: 720, source: 'Getty', format: 'jpg')
     assert_price_equal 1, get_product_price(image[:id])
   end
 
-  test 'images from Getty are sold at 3 when below 1920x1080' do
+  test 'prices images from Getty at 3 when below 1920x1080' do
     image1 = repo.create_image(title: 'Title of Image1', width: 1281, height: 720, source: 'Getty', format: 'jpg')
     assert_price_equal 3, get_product_price(image1[:id])
 
@@ -84,12 +84,12 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
     assert_price_equal 3, get_product_price(image2[:id])
   end
 
-  test 'images from Getty are sold at 5 when above 1920x1080' do
+  test 'prices images from Getty at 5 when above 1920x1080' do
     image = repo.create_image(title: 'Title of Image', width: 1921, height: 1080, source: 'Getty', format: 'jpg')
     assert_price_equal 5, get_product_price(image[:id])
   end
 
-  test 'images from Getty in raw format are at 10' do
+  test 'prices images from Getty in raw format at 10' do
     image1 = repo.create_image(title: 'Title of Image1', width: 800, height: 600, source: 'Getty', format: 'raw')
     image2 = repo.create_image(title: 'Title of Image2', width: 1920, height: 1080, source: 'Getty', format: 'raw')
     image3 = repo.create_image(title: 'Title of Image3', width: 1921, height: 1080, source: 'Getty', format: 'raw')
@@ -99,18 +99,18 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
     assert_price_equal 10, get_product_price(image3[:id])
   end
 
-  test 'price of another image' do
+  test 'prices other images at 7' do
     image = repo.create_image(title: 'Title of Image', width: 800, height: 600, source: 'unknown', format: 'jpg')
     assert_price_equal 7, get_product_price(image[:id])
   end
 
-  test 'price of 4k videos' do
+  test 'prices 4k videos at 0.08/second' do
     Timecop.travel Time.new(2022, 1, 1) + 6.hours
     video = repo.create_video(title: 'Title of Video', duration: 150, quality: '4k')
     assert_price_equal 12, get_product_price(video[:id])
   end
 
-  test 'price of FullHD videos is 3 per started minute' do
+  test 'prices FullHD videos at 3 per started minute' do
     Timecop.travel Time.new(2022, 1, 1) + 6.hours
     video60 = repo.create_video(title: 'Title of Video', duration: 59, quality: 'FullHD')
     assert_price_equal 3, get_product_price(video60[:id])
@@ -118,7 +118,7 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
     assert_price_equal 6, get_product_price(video61[:id])
   end
 
-  test 'price of SD videos is 1 per started minute' do
+  test 'prices SD videos at 1 per started minute' do
     Timecop.travel Time.new(2022, 1, 1) + 6.hours
     video60 = repo.create_video(title: 'Title of Video', duration: 59, quality: 'SD')
     assert_price_equal 1, get_product_price(video60[:id])
@@ -126,13 +126,13 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
     assert_price_equal 2, get_product_price(video61[:id])
   end
 
-  test 'price of SD videos longer than 10 minutes is fixed at 10' do
+  test 'prices SD videos longer than 10 minutes at 10' do
     Timecop.travel Time.new(2022, 1, 1) + 6.hours
     video = repo.create_video(title: 'Title of Video', duration: 10*60, quality: 'SD')
     assert_price_equal 10, get_product_price(video[:id])
   end
 
-  test 'the price of a video is reduced during the night' do
+  test 'reduces video prices by -40% during the night' do
     video = repo.create_video(title: 'Title of Video', duration: 150, quality: '4k')
     Timecop.travel Time.new(2022, 1, 1) + 5.hours - 1.minute
     assert_price_equal 7.2, get_product_price(video[:id])
@@ -144,7 +144,7 @@ class ProductPricesControllerTest < ActionDispatch::IntegrationTest
     assert_price_equal 7.2, get_product_price(video[:id])
   end
 
-  test 'the price is bumped +5% if the title contains "premium"' do
+  test 'bumps price of any product by +5% if the title contains "premium"' do
     book1 = repo.create_book(title: 'Title of Book', isbn: '1', purchase_price: 12, is_hot: false)
     assert_price_equal 15, get_product_price(book1[:id])
     book = repo.create_book(title: 'Title of premium Book', isbn: '1', purchase_price: 12, is_hot: false)
