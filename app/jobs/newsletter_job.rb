8220c9695894f4c285a3b1ca5fc3069e1da44b54
Loïc Knuchel <loic.knuchel@doctolib.com>
2022-06-14 15:23:24 +0200

    add iteration 5 impl

--- app/jobs/newsletter_job.rb
+++ app/jobs/newsletter_job.rb
@@ -1,5 +1,24 @@
 class NewsletterJob < ApplicationJob
   def perform(mail_sender)
-    # FIXME at iteration 5
+    products = ProductRepository.new.get_all_products
+    mail_contents = products.group_by { |product| product[:kind] }.map do |kind, kind_products|
+      <<~PRODUCT
+        #{kind}s
+        #{kind_products.map { |product| product_section(product) }.join("\n")}
+      PRODUCT
+    end.join
+    mail_sender.send_newsletter(mail_contents)
+  end
+
+  private
+
+  def product_section(product)
+    details = case product[:kind]
+              when 'book' then "#{product[:isbn]}"
+              when 'image' then "#{product[:width]}x#{product[:height]}"
+              when 'video' then "#{product[:duration]} seconds"
+              else raise "Unknown product kind #{product[:kind]}"
+              end
+    "#{product[:title]} - #{details}"
   end
 end
