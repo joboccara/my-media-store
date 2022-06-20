5e413bb286934c9fb3b06878c3c5614d526eb9c6
Michel Domenjoud <michel.domenjoud@doctolib.com>
2022-06-20 20:19:32 +0200

    enforce to use "to" parameter in newsletter job

--- test/controllers/iteration_5_test.rb
+++ test/controllers/iteration_5_test.rb
@@ -7,7 +7,7 @@ class Iteration5Test < TestHelperTraining
     create_book(title: 'Refactoring: Improving the Design of Existing Code', isbn: '9780134757599', purchase_price: 42, is_hot: false, created_at: Time.parse('2021-04-05'))
     create_image(title: 'XKCD: Good code', width: 800, height: 600, source: 'unknown', format: 'jpg', created_at: Time.parse('2022-05-19 19:32'))
     create_video(title: 'Living Documentation : you will like documentation', duration: 120, quality: 'HD', created_at: Time.parse('2022-05-23 13:01'))
-    create_video(title: 'What is DDD - Eric Evans - DDD Europe', duration: 120, quality: 'HD', created_at: Time.parse('2022-06-07 09:18'))
+    create_video(title: 'What is DDD - Eric Evans - DDD Europe', duration: 120, quality: 'HD', created_at: Time.parse('2022-05-31 09:18'))
 
     expected_mail_content = <<~MAIL
     Books
@@ -20,7 +20,7 @@ class Iteration5Test < TestHelperTraining
 
     mail_sender = mock
     mail_sender.expects(:send_newsletter).with(expected_mail_content)
-    NewsletterJob.perform_now(from: '2022-05-01', to: '2022-05-31', mail_sender: mail_sender)
+    NewsletterJob.perform_now(from: '2022-05-01', to: '2022-05-30', mail_sender: mail_sender)
   end
 
   test 'gets an image with its details from an external service' do
