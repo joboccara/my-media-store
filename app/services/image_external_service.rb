c136bcb847818d39758fde56576366691b136fa2
joboccara <boccaraj@gmail.com>
2022-06-20 17:26:33 +0200

    Image external service test

---
+++ app/services/image_external_service.rb
@@ -0,0 +1,13 @@
+module ImageExternalService
+  def self.get_image_details(id)
+    # DON'T WORRY ABOUT IMPLEMENTING THIS METHOD, IT IS MOCKED IN THE TESTS
+    # call external service...
+    # { width: 42, height: 42 }
+  end
+
+  def self.upload_image_details(width:, height:)
+    # DON'T WORRY ABOUT IMPLEMENTING THIS METHOD, IT IS MOCKED IN THE TESTS
+    # call external service...
+    # { id: 42 }
+  end
+end
