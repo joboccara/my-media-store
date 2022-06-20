73007ef034a9a0e1817db15a95d60ad03647e57f
Michel Domenjoud <michel.domenjoud@doctolib.com>
2022-06-20 17:09:37 +0200

    add range in newsletter parameters

--- app/jobs/newsletter_job.rb
+++ app/jobs/newsletter_job.rb
@@ -1,5 +1,5 @@
 class NewsletterJob < ApplicationJob
-  def perform(mail_sender)
+  def perform(from:, to:, mail_sender:)
     # FIXME at iteration 5
   end
 end
--- test/controllers/iteration_5_test.rb
+++ test/controllers/iteration_5_test.rb
@@ -3,21 +3,23 @@ require "test_helper_training"
 class Iteration5Test < TestHelperTraining
   test 'it sends a newsletter with the existing products' do
     skip 'unskip at iteration 5'
-    create_book(title: 'Software craft', isbn: '2100825208', purchase_price: 42, is_hot: false)
-    create_image(title: 'XKCD: Good code', width: 800, height: 600, source: 'unknown', format: 'jpg')
-    create_video(title: 'Living Documentation : vous allez aimer la documentation', duration: 120, quality: 'HD')
+    create_book(title: 'Software craft', isbn: '2100825208', purchase_price: 42, is_hot: false, created_at: Time.parse('2022-05-05 09:18'))
+    create_book(title: 'Refactoring: Improving the Design of Existing Code', isbn: '9780134757599', purchase_price: 42, is_hot: false, created_at: Time.parse('2021-04-05'))
+    create_image(title: 'XKCD: Good code', width: 800, height: 600, source: 'unknown', format: 'jpg', created_at: Time.parse('2022-05-19 19:32'))
+    create_video(title: 'Living Documentation : you will like documentation', duration: 120, quality: 'HD', created_at: Time.parse('2022-05-23 13:01'))
+    create_video(title: 'What is DDD - Eric Evans - DDD Europe', duration: 120, quality: 'HD', created_at: Time.parse('2022-06-07 09:18'))
 
     expected_mail_content = <<~MAIL
-    books
-    Software craft - 2100825208
-    images
-    XKCD: Good code - 800x600
-    videos
-    Living Documentation : vous allez aimer la documentation - 120 seconds
+    Books
+    * Software craft - 2100825208 - 52.50 € 
+    Images
+    * XKCD: Good code - 800x600 - 7 €
+    Videos
+    * Living Documentation : you will like documentation - 120 seconds - 15 €
     MAIL
 
     mail_sender = mock
     mail_sender.expects(:send_newsletter).with(expected_mail_content)
-    NewsletterJob.perform_now(mail_sender)
+    NewsletterJob.perform_now(from: '2022-05-01', to: '2022-05-31', mail_sender: mail_sender)
   end
 end
