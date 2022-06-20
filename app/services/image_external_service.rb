bcf4d8648315aea7ba154066f68648b28066f18a
joboccara <boccaraj@gmail.com>
2022-06-20 17:30:37 +0200

    Complete image external service test

--- app/services/image_external_service.rb
+++ app/services/image_external_service.rb
@@ -2,10 +2,10 @@ module ImageExternalService
   def self.get_image_details(id)
     # DON'T WORRY ABOUT IMPLEMENTING THIS METHOD, IT IS MOCKED IN THE TESTS
     # call external service...
-    # { width: 42, height: 42 }
+    # { width: 800, height: 600, source: 'Getty', format: 'jpg' }
   end
 
-  def self.upload_image_details(width:, height:)
+  def self.upload_image_details(width:, height:, source:, format:)
     # DON'T WORRY ABOUT IMPLEMENTING THIS METHOD, IT IS MOCKED IN THE TESTS
     # call external service...
     # { id: 42 }
